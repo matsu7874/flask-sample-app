1917ac8127c5639b32ef69c294f9608efdd5f9c4
matsu7874 <mtsmtkmt@gmail.com>
2018-09-06 02:12:09 +0900

    format

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, request
-app = Flask(__name__)
+import flask
+
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/user/<name>', methods=['GET'])
 def get_user(name=None):
-
     return 'Hello, World! Hello {name}.'.format(name=name)
 
+
 @app.route('/search', methods=['GET'])
-def get_search(name=''):
-    query = request.args.get('q')
+def get_search():
+    query = flask.request.args.get('q')
     return 'No matches were found for "{query}".'.format(query=query)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
